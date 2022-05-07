235a5c6b842e55bd3b0cd6be2907eeea2aa5669f
John Lloyd Mamauag <mamauagjohnlloyd98@gmail.com>
2022-05-08 04:19:03 +0800

    First commit

--- connect.py
+++
@@ -1,4 +0,0 @@
-class connect():
-    def converter(val):
-        km=0.62*val
-        return km
--- test.py
+++
@@ -1,4 +0,0 @@
-from distutils.util import convert_path
-from lib2to3.pytree import convert
-import connect as con
-
